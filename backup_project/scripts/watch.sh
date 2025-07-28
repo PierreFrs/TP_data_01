ffca81fa5f51546dea7f145bcfbc295526145db1
pfraisse <pierrefraisse90@gmail.com>
2025-07-28 09:44:13 +0200

    feat: Finished step 3

--- backup_project/scripts/watch.sh
+++ backup_project/scripts/watch.sh
@@ -1,1 +1,20 @@
-#! /bin/bash
+#!/bin/bash
+
+DIRECTORY="../data/work/"
+
+while true
+do
+    echo "Checking work directory..."
+    if [ "$(ls -A $DIRECTORY 2>/dev/null)" ]; then
+        echo "Files detected, launching jobs..."
+        for f in "$DIRECTORY"*.* ; do
+            [[ -e "$f" ]] || break
+            filename=$(basename "$f")
+            echo "Processing: $filename"
+            ./backup.sh "$filename"
+        done
+    else
+        echo "No file detected in work directory"
+    fi
+    sleep 30
+done
